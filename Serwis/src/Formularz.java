cac8687b63e3b935bf96272d27fe99b2c8127237
Mireczek <mirek2220@gmail.com>
2014-01-12 17:10:55 +0100

    Dodane usuwanie samochodu

--- Serwis/src/Formularz.java
+++ Serwis/src/Formularz.java
@@ -1,6 +1,8 @@
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 
 public class Formularz extends JFrame{
@@ -9,6 +11,12 @@ public class Formularz extends JFrame{
 		setPreferredSize(new Dimension(200,500));
 		setLocation(Frame.sWidth/2,Frame.sHeight/2);
 		setVisible(true);
+		JLabel jl = new JLabel("Numer Samochodu : ");
+		JLabel jl2 = new JLabel("Data : ");
+		JLabel jl3 = new JLabel("Opis us³ugi : ");
+		JTextArea tx1 = new JTextArea();
+		JTextArea tx2 = new JTextArea();
+		JTextArea tx3 = new JTextArea();
 	
 	}
 	
